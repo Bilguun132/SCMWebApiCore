8645e2b028b1e3e40141bc4fc895731b94608d32
Bilguun132 <Bilguun132@gmail.com>
2018-07-09 15:33:17 +0800

    added secret key

---
+++ SCMWebApiCore/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SCMWebApiCore.DataProviders;
+using SCMWebApiCore.Models;
+using System.Data.SqlClient;
+
+namespace SCMWebApiCore
+{
+    public class Startup
+    {
+        readonly bool isDebug = false;
+        private string _connection = null;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IDataProvider, DataProvider>();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowCredentials()
+                       .AllowAnyHeader();
+            }));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            _connection = Configuration.GetConnectionString("AzureConnectionString");
+            services.AddDbContext<SCM_GAMEContext>(options => options.UseSqlServer(_connection));
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors("MyPolicy");
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chat");
+            });
+            app.UseMvc();
+        }
+    }
+}
